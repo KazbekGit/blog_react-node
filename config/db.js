93f424225504987ad88d65e3ec5e6c0b4f481c31
Kazbekgit <kazbekgit@gmail.com>
2024-09-08 22:39:44 +0300

    add comments in connectToDatabase.js

--- config/db.js
+++ config/db.js
@@ -4,6 +4,10 @@ import {config} from 'dotenv';
 
 config()
 
+/**
+ * Connects to the MongoDB database using the connection string from the environment variables.
+ * Logs a success message to the console if the connection is successful, or an error message if the connection fails.
+ */
 export const connectToDatabase = async () => {
     try {
         await mongoose.connect(pathDB);
